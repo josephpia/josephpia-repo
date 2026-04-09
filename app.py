7c4ec061f987673b1b01e4e535cb9f2ab10d7e31
IbanezCyriljay <cyriljay.ibanez@bisu.edu.ph>
2026-04-09 22:58:42 +0800

    admindashboard have a user info and recieve service request sign up collect user info and save user can input a service request

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = "secretkey123"
 
-# Default accounts
+# Default admin account
 users = {
-    "admin": {"password": "1234", "role": "admin"}
+    "admin": {
+        "password": "1234",
+        "role": "admin"
+    }
 }
 
 login_count = 0
+service_requests = []
 
 @app.route('/')
 def home():
@@ -26,6 +30,7 @@ def login():
 
         if username not in users:
             message = "User does not have an account"
+
         elif users[username]["password"] == password:
             session['username'] = username
             session['role'] = users[username]["role"]
@@ -36,6 +41,7 @@ def login():
                 return redirect(url_for('admin_dashboard'))
             else:
                 return redirect(url_for('user_dashboard'))
+
         else:
             message = "Wrong password"
 
@@ -47,44 +53,63 @@ def signup():
     message = ""
 
     if request.method == 'POST':
+        firstname = request.form['firstname']
+        middlename = request.form['middlename']
+        lastname = request.form['lastname']
+        age = request.form['age']
+        address = request.form['address']
+        birthdate = request.form['birthdate']
+        email = request.form['email']
+        cellphone = request.form['cellphone']
         username = request.form['username']
         password = request.form['password']
 
         if username in users:
             message = "Username already exists"
+
         else:
-            users[username] = {"password": password, "role": "user"}
+            users[username] = {
+                "firstname": firstname,
+                "middlename": middlename,
+                "lastname": lastname,
+                "age": age,
+                "address": address,
+                "birthdate": birthdate,
+                "email": email,
+                "cellphone": cellphone,
+                "password": password,
+                "role": "user"
+            }
+
             return redirect(url_for('login'))
 
     return render_template('signup.html', message=message)
 
 # USER DASHBOARD
-@app.route('/userdashboard')
+@app.route('/userdashboard', methods=['GET', 'POST'])
 def user_dashboard():
-    return render_template('userdashboard.html')
+    message = ""
 
-# ADMIN MAIN DASHBOARD
+    if request.method == 'POST':
+        service = request.form['service']
+        service_requests.append({
+            "username": session['username'],
+            "service": service
+        })
+        message = "Service request submitted"
+
+    return render_template('userdashboard.html', message=message)
+
+# ADMIN DASHBOARD
 @app.route('/admindashboard')
 def admin_dashboard():
     total_users = len(users)
-    return render_template('admindashboard.html', total_users=total_users)
-
-# APP MONITOR
-@app.route('/appmonitor')
-def app_monitor():
-    total_users = len(users)
-    active_users = total_users - 1
-    return render_template('appmonitor.html', total_users=total_users, active_users=active_users)
-
-# HISTORY
-@app.route('/history')
-def history():
-    return render_template('history.html', login_count=login_count)
-
-# SETTINGS
-@app.route('/settings')
-def settings():
-    return render_template('settings.html')
+    return render_template(
+        'admindashboard.html',
+        total_users=total_users,
+        users=users,
+        service_requests=service_requests
+    )
 
 # LOGOUT
 @app.route('/logout')
